8531ef63acb3998e1f1c991691d7d105e3f8138a
richardnj14 <richardnjanssen@gmail.com>
2026-06-16 17:13:54 +0200

    First version

---
+++ operacoes_aritmeticas.py
@@ -0,0 +1,17 @@
+a = int(input("a: "))
+b = int (input("b: "))
+
+soma = a + b
+sub = a - b
+mult = a*b
+div = a/b
+divint = a//b
+resto = a%b
+
+print()
+print(f"{a} + {b} = {soma}")
+print(f"{a} - {b} = {sub}")
+print(f"{a} * {b} = {mult}")
+print(f"{a} / {b} = {div}")
+print(f"{a} // {b} = {divint}")
+print(f"{a} % {b} = {resto}")
