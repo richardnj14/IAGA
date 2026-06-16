c32e38bcfcb04f47f0fd78b7f710061184246246
richardnj14 <richardnjanssen@gmail.com>
2026-06-16 17:26:47 +0200

    First version

--- operacoes_aritmeticas.py
+++ operacoes_aritmeticas.py
@@ -8,8 +8,7 @@ div = a/b
 divint = a//b
 resto = a%b
 
-print()
-print(f"{a} + {b} = {soma}")
+print(f"\n{a} + {b} = {soma}")
 print(f"{a} - {b} = {sub}")
 print(f"{a} * {b} = {mult}")
 print(f"{a} / {b} = {div}")
